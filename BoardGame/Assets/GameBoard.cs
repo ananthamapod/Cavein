4cf1945719f435e310e71fbce7066840ef32502c
ViggyNash <viggyviggynash@gmail.com>
2016-02-12 19:15:40 -0500

    Prototype files added.

---
+++ BoardGame/Assets/GameBoard.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using CoherentNoise;
+using CoherentNoise.Generation;
+
+public class GameBoard : MonoBehaviour {
+
+    public GameObject background;
+    public GameObject tilePrefab;
+
+    private List<List<GameObject>> cubeGrid;
+    public int gridHeight;
+    public int gridWidth;
+
+    private Generator heightMap;
+    private float threshold;
+
+    private List<List<GameObject>> boardMap = new List<List<GameObject>>();
+
+	// Use this for initialization
+	void Start () {
+        cubeGrid = GetComponent<RandomizedGrid>().PopulateGrid(gridHeight, gridWidth);
+        heightMap = GetComponent<RandomizedGrid>().CreateGenerator();
+        threshold = GetComponent<RandomizedGrid>().threshold;
+        GenerateBoardMap();
+    }
+
+    void GenerateBoardMap()
+    {
+        background = Instantiate(background);
+        background.name = "Background";
+        background.transform.localScale = new Vector3(((float)gridHeight * 1.05f) / 10, 0, ((float)gridWidth * 1.05f) / 10);
+        background.transform.position = new Vector3(.5f, 0, .5f);
+
+        for (int x = 0; x < gridHeight; x += 2)
+        {
+            List<GameObject> sublist = new List<GameObject>();
+            for (int y = 0; y < gridWidth; y += 2)
+            {
+                if (TestLocation(x,y))
+                {
+                    Debug.Log(x + " " + y);
+                    GameObject tile = Instantiate(tilePrefab);
+                    sublist.Add(tile);
+                    tile.transform.position = new Vector3(gridHeight / 2 - x - .5f, 3, gridWidth / 2 - y - .5f);
+                }
+            }
+            boardMap.Add(sublist);
+        }
+    }
+
+    bool TestLocation(int x, int y)
+    {
+        return (heightMap.GetValue(x, y, 0) < threshold) && (heightMap.GetValue(x + 1, y, 0) < threshold) && (heightMap.GetValue(x, y + 1, 0) < threshold) && (heightMap.GetValue(x + 1, y + 1, 0) < threshold);
+    }
+
+    public class Board
+    {
+        public class Tile
+        {
+            public bool occupied;
+            GameObject Character;
+        }
+
+        List<List<Tile>> tileMap = new List<List<Tile>>();6
+
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ BoardGame/Assets/RandomizedGrid.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using CoherentNoise;
+using CoherentNoise.Generation;
+using CoherentNoise.Generation.Voronoi;
+using CoherentNoise.Generation.Displacement;
+using CoherentNoise.Generation.Fractal;
+using CoherentNoise.Generation.Modification;
+using CoherentNoise.Generation.Patterns;
+
+public class RandomizedGrid : MonoBehaviour {
+
+    // Basic grid data
+    public List<List<GameObject>> cubeGrid = new List<List<GameObject>>();
+    //public int height;
+    //public int width;
+    public Material cubeColorBase;
+
+    // Randomization data
+   // private List<List<float>> heightMap = new List<List<float>>();
+    public int seed;
+    public float threshold = .6f;
+
+    // Use this for initialization
+    /*void Start () {
+        PopulateGrid();
+    }*/
+
+    public List<List<GameObject>> PopulateGrid(int height, int width)
+    {
+        Generator noise = CreateGenerator();
+
+        for (int y = 0; y < height; y++)
+        {
+            List<GameObject> cubeGridSub = new List<GameObject>();
+            cubeGrid.Add(cubeGridSub);
+            for (int x = 0; x < width; x++)
+            {
+                float noiseVal = noise.GetValue(x, y, 0);
+                if (noiseVal < threshold)
+                {
+                    continue;
+                }
+
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                //cube.AddComponent<Rigidbody>();
+                //Debug.Log(noiseVal);
+                cube.transform.position = new Vector3(height / 2 - x, (noiseVal - threshold) / 2, width / 2 - y);
+                cube.transform.localScale = new Vector3(1, noiseVal - threshold, 1);
+
+                Color cubeColor;
+				cubeColor = Color.white;//new Color((noiseVal - threshold) * 10, (noiseVal - threshold) * 10, (noiseVal - threshold) * 10);
+
+                Material newMat = new Material(cubeColorBase);
+                newMat.SetColor("_Color", cubeColor);
+                cube.GetComponent<MeshRenderer>().material = newMat;
+
+                cubeGridSub.Add(cube);
+            }
+        }
+        return cubeGrid;
+    }
+
+    // Defines the noise generator to be used
+    public Generator CreateGenerator()
+    {
+        PinkNoise baseNoise = new PinkNoise(seed);
+        baseNoise.Persistence = .05f;
+        baseNoise.Frequency = .1f;
+        Generator baseNoiseGain = baseNoise.Gain(0f);
+
+        VoronoiValleys2D voronoiMod = new VoronoiValleys2D(seed);
+        voronoiMod.Frequency = .05f;
+        Generator voronoiModGain = voronoiMod.Gain(.5f);
+
+        Generator noise = voronoiModGain * baseNoiseGain;
+        //noise.Binarize(threshold);
+
+        noise = noise.ScaleShift(1f, .5f);
+        noise = noise.ScaleShift(5, 0);
+
+        return noise;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
